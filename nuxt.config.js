539f3256880f0a22aa36ffdc93eb1ca98a677610
florlovingmemory <93557099+florlovingmemory@users.noreply.github.com>
2021-11-01 19:14:47 -0400

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,5 @@
-const name = 'Janet Doe'
-const dates = 'July 29th 1958 - Jan 4th 2021'
+const name = 'Flor Q'
+const dates = '[Placeholder] - October 29th, 2021'
 
 export default {
   publicRuntimeConfig: {
