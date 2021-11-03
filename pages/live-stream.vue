54659154147e9e326b0e7030d1b3dc3d51637786
florlovingmemory <93557099+florlovingmemory@users.noreply.github.com>
2021-11-02 21:18:39 -0400

    Update live-stream.vue

--- pages/live-stream.vue
+++ pages/live-stream.vue
@@ -6,12 +6,12 @@
     <div class="section details">
       <h1 class="title">Service Livestream</h1>
       <p>As attendance is restricted due to COVID-19 we hope that you will join us virtually. The services will be held on <em>Thursday January 28th 2021</em>, first at Church address then at Crematorium address.</p>
-      <h2>Church Service</h2>
-      <p>The church service is being performed at Church address
+      <h2>Religious Service</h2>
+      <p>The Religious service is [info]
       </p>
       <a class="button" target="_blank" rel=”noopener” href="">Livestream - 10:30am</a>
-      <h2>Crematorium</h2>
-      <p>The service is being held at Crematorium address</p>
+      <h2>Additional Info</h2>
+      <p>Placeholder for future [info]</p>
       <a class="button" target="_blank" rel=”noopener” href="">Livestream - 12:00pm</a>
       <ul class="fa-ul">
         <ListItem>Username: <span class="highlight">test123</span> <CopyButton text="test123"></CopyButton></ListItem>
