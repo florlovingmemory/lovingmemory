9ad9b26eeee226f7a8825cd8a26c164af9d0a829
florlovingmemory <93557099+florlovingmemory@users.noreply.github.com>
2021-11-02 20:45:11 -0400

    Update order-of-service.vue

--- pages/order-of-service.vue
+++ pages/order-of-service.vue
@@ -4,9 +4,9 @@
       <Nav />
     </div>
     <div class="section details">
-      <h1 class="title">Order of service</h1>
-      <p><em>Thursday 28th January 2021</em> Due to COVID-19 the services are restricted in the number of people allowed. The plan is to live stream the services so everyone who wants to attend will be able to do so virtually. We will also hold a celebration of Lyns life later in the year when COVID-19 restrictions are relaxed.</p>
-      <p class="highlight">A Bit of important text that you want to stand out, such as any dressing requirements.</p>
+      <h1 class="title">Obituary</h1>
+      <p><em>[PLACEHOLDER DATE]</em> [PLACEHOLDER TEXT FOR OBITUARY SUMMARY]</p>
+      <--! COMMENT
       <h2>Church Service <span>10:30am</span></h2>
       <ul class="fa-ul">
         <ListItem>Service starts at 10:30am at Church Address</ListItem>
@@ -34,6 +34,7 @@
         <ListItem>Closing prayer &amp; Grace blessing</ListItem>
         <ListItem>Exit music: New Wine</ListItem>
       </ul>
+      COMMENT END-->
     </div>
   </div>
 </template>
