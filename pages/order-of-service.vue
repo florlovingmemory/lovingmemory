1288dca7b3d13c5e61df68a974344cde00f50205
florlovingmemory <93557099+florlovingmemory@users.noreply.github.com>
2021-11-02 21:16:55 -0400

    Update order-of-service.vue

--- pages/order-of-service.vue
+++ pages/order-of-service.vue
@@ -6,35 +6,8 @@
     <div class="section details">
       <h1 class="title">Obituary</h1>
       <p><em>[PLACEHOLDER DATE]</em> [PLACEHOLDER TEXT FOR OBITUARY SUMMARY]</p>
-      /*<--! COMMENT
-      <h2>Church Service <span>10:30am</span></h2>
-      <ul class="fa-ul">
-        <ListItem>Service starts at 10:30am at Church Address</ListItem>
-        <ListItem>Entrance music: And Can It Be</ListItem>
-        <ListItem>Scripture reading</ListItem>
-        <ListItem>Welcome &amp; Opening Prayer</ListItem>
-        <ListItem>Song: Goodness of God</ListItem>
-        <ListItem>Tributes - Family &amp; Friends</ListItem>
-        <ListItem>Few moments of quiet</ListItem>
-        <ListItem>Short message</ListItem>
-        <ListItem>Prayer of Commendation</ListItem>
-        <ListItem>Closing prayer &amp; Grace blessing</ListItem>
-        <ListItem>Exit music: New Wine</ListItem>
-      </ul>
-      <h2>Crematorium <span>12:00pm</span></h2>
-      <ul class="fa-ul">
-        <ListItem>Service starts at 12pm at Crematorium address</ListItem>
-        <ListItem>Entrance music: And Can It Be</ListItem>
-        <ListItem>Scripture reading - Psalm 61 v1-4</ListItem>
-        <ListItem>Welcome &amp; Opening Prayer</ListItem>
-        <ListItem>Tribute</ListItem>
-        <ListItem>Few moments of quiet</ListItem>
-        <ListItem>Short message</ListItem>
-        <ListItem>Committal</ListItem>
-        <ListItem>Closing prayer &amp; Grace blessing</ListItem>
-        <ListItem>Exit music: New Wine</ListItem>
-      </ul>
-      COMMENT END-->*/
+      <h2>[HEADING]<span>10:30am</span></h2>
+      <h2>[HEADING]<span>[]</span></h2>
     </div>
   </div>
 </template>
