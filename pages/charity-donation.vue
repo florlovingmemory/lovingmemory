32da58c792e0b7d47a53aba8c7282f651f155b7a
florlovingmemory <93557099+florlovingmemory@users.noreply.github.com>
2021-11-02 21:26:22 -0400

    Update charity-donation.vue

--- pages/charity-donation.vue
+++ pages/charity-donation.vue
@@ -4,16 +4,15 @@
       <Nav />
     </div>
     <div class="section details">
-      <h1 class="title">Charity Donations</h1>
-      <p>Instead of flowers we would like to suggest people make a donation to one of the following charities</p>
-      <h2>Marie Curie</h2>
-      <p>We are so grateful to <em>Marie Curie</em> for their support and making sure their patients are as 
-        comfortable and peaceful as possible when the end came.
+      <h1 class="title">Donations</h1>
+      <p>[Insert future charity memo and info.]</p>
+      <h2>[Heading]</h2>
+      <p>[Donation information and instructions]
       </p>
-      <a class="button" target="_blank" rel=”noopener” href="https://www.justgiving.com/fundraising/">Donate to Marie Curie</a>
-      <h2>Make a Wish</h2>
-      <p><em>Make A Wish</em>'s goal of giving joy to children during a terrible time is something that would be a fitting tribute.</p>
-      <a class="button" target="_blank" rel=”noopener” href="https://www.justgiving.com/fundraising/">Donate to Make a Wish</a>
+      <a class="button" target="_blank" rel=”noopener” href="sms:555888">[Test Apple Pay via Message]</a>
+      <h2>[HEADING]</h2>
+      <p>[Insert future charity memo and info.]</p>
+      <a class="button" target="_blank" rel=”noopener” href="https://florlovingmemory.github.io/lovingmemory/">[Test for future method]</a>
     </div>
   </div>
 </template>
