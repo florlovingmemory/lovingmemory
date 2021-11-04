79e757cf28391fe3bd26ec295e52477ce8c116cd
florlovingmemory <93557099+florlovingmemory@users.noreply.github.com>
2021-11-04 19:49:22 -0400

    Update live-stream.vue

--- pages/live-stream.vue
+++ pages/live-stream.vue
@@ -5,19 +5,9 @@
     </div>
     <div class="section details">
       <h1 class="title">Service Livestream</h1>
-      <p>As attendance is restricted due to COVID-19 we hope that you will join us virtually. The services will be held on <em>Thursday January 28th 2021</em>, first at Church address then at Crematorium address.</p>
-      <h2>Religious Service</h2>
-      <p>The Religious service is [info]
-      </p>
-      <a class="button" target="_blank" rel=”noopener” href="">Livestream - 10:30am</a>
-      <h2>Additional Info</h2>
-      <p>Placeholder for future [info]</p>
-      <a class="button" target="_blank" rel=”noopener” href="">Livestream - 12:00pm</a>
-      <ul class="fa-ul">
-        <ListItem>Username: <span class="highlight">test123</span> <CopyButton text="test123"></CopyButton></ListItem>
-        <ListItem>Password: <span class="highlight">password123</span> <CopyButton text="password123"></CopyButton></ListItem>
-      </ul>
-
+      <p>As attendance is restricted due to COVID-19 we hope that you will join us virtually. The services will be held on Sunday, November 14th 2021.</p>
+      <h1 class="title">Service en vivo</h1>
+      <p>Como la asistencia está restringida debido a COVID-19, esperamos que se una a nosotros virtualmente. Los servicios se llevarán a cabo el domingo 14 de noviembre de 2021.</p>
     </div>
   </div>
 </template>
