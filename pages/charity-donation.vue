03187e716550fe51093f7519dca5d19726138b80
florlovingmemory <93557099+florlovingmemory@users.noreply.github.com>
2021-11-03 22:09:16 -0400

    Update charity-donation.vue

--- pages/charity-donation.vue
+++ pages/charity-donation.vue
@@ -5,10 +5,8 @@
     </div>
     <div class="section details">
       <h1 class="title">Donations</h1>
-      <p>If anyone would like to donate to the family, you can donate through Zelle. Please send to Jessica Escalante's phone number at 516-724-5225.</p>
-      <a class="button" target="_blank" rel=”noopener” href="tel:5167245225">[Test Apple Pay via Message]</a>
-      <p>Si alguien desea donar a la familia, puede donar a través de Zelle. Envíe al número de teléfono de Jessica Escalante al 516-724-5225.</p>
-      <a class="button" target="_blank" rel=”noopener” href="tel:5167245225">[Test for future method]</a>
+      <p>If anyone would like to donate to the family, you can donate through Zelle. Please send to Jessica Escalante's phone number at 516-724-5225. For any other kind of donation please contact Jessica at 516-724-5225.</p>
+      <p>Si alguien desea donar a la familia, puede donar a través de Zelle. Envíe al número de teléfono de Jessica Escalante al 516-724-5225. Para cualquier otro tipo de donación, comuníquese con Jessica al 516-724-5225.</p>
     </div>
   </div>
 </template>
