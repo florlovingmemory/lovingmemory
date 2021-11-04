030fa9e9d654ec82e75a77a08d3adf39862a966b
florlovingmemory <93557099+florlovingmemory@users.noreply.github.com>
2021-11-03 20:34:12 -0400

    Update charity-donation.vue

--- pages/charity-donation.vue
+++ pages/charity-donation.vue
@@ -5,14 +5,10 @@
     </div>
     <div class="section details">
       <h1 class="title">Donations</h1>
-      <p>[Insert future charity memo and info.]</p>
-      <h2>[Heading]</h2>
-      <p>[Donation information and instructions]
-      </p>
-      <a class="button" target="_blank" rel=”noopener” href="sms:555888">[Test Apple Pay via Message]</a>
-      <h2>[HEADING]</h2>
-      <p>[Insert future charity memo and info.]</p>
-      <a class="button" target="_blank" rel=”noopener” href="https://florlovingmemory.github.io/lovingmemory/">[Test for future method]</a>
+      <p>If anyone would like to donate to the family, you can donate through Zelle. Please send to Jessica Escalante's phone number at 516-724-5225.</p>
+      <a class="button" target="_blank" rel=”noopener” href="tel:5167245225">[Test Apple Pay via Message]</a>
+      <p>Si alguien desea donar a la familia, puede donar a través de Zelle. Envíe al número de teléfono de Jessica Escalante al 516-724-5225.</p>
+      <a class="button" target="_blank" rel=”noopener” href="tel:5167245225">[Test for future method]</a>
     </div>
   </div>
 </template>
