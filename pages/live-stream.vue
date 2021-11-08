3cceb1264ad94aef091006893d3841a099d83042
florlovingmemory <93557099+florlovingmemory@users.noreply.github.com>
2021-11-07 20:32:42 -0500

    Update live-stream.vue

--- pages/live-stream.vue
+++ pages/live-stream.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="section details">
       <h1 class="title">Service Livestream</h1>
-      <p>As attendance is restricted due to COVID-19 we hope that you will join us virtually. The services will be held on Sunday, November 14th 2021. Please check back on November 14, 2021 for the zoom link</p>
+      <p>As attendance is restricted due to COVID-19 we hope that you will join us virtually. The services will be held on Sunday, November 14th 2021. Please check back on November 14, 2021 for the zoom link.</p>
       <h1 class="title">Servicio en vivo</h1>
       <p>Como la asistencia está restringida debido a COVID-19, esperamos que se una a nosotros virtualmente. Los servicios se llevarán a cabo el domingo 14 de noviembre de 2021. Por favor vuelva a consultar el 14 de noviembre de 2021 para ver el enlace de zoom.
 </p>
