92fef8d82fb29d6fb96851b1c7e50aab1eace4cf
florlovingmemory <93557099+florlovingmemory@users.noreply.github.com>
2021-11-08 20:53:37 -0500

    Update live-stream.vue

--- pages/live-stream.vue
+++ pages/live-stream.vue
@@ -14,9 +14,9 @@
       Password: flor7 </p>
       <h1 class="title">Servicio en vivo</h1>
       <p>Discurso en memoria de Flor Quintero</p>
-      <p>Date: Sunday November 14, 2021<br>
-      Time: 2:30 PM</p>
-      <p>Link:<br>
+      <p>Fecha: Domingo 14 de noviembre de 2021<br>
+      Hora: 2:30 PM</p>
+      <p>Enlace:<br>
       <a href="https://us02web.zoom.us/j/89968145478?pwd=OUtWM0ZTK25rbWM2cmRMeC8rSXV3dz09">https://us02web.zoom.us/j/89968145478?pwd=OUtWM0ZTK25rbWM2cmRMeC8rSXV3dz09</a></p>
       <p>ID: 899 6814 5478<br>
       Password: flor7 </p>
